54f2dbd9b220c3a645ff3037aa5661cd0e6a4675
Rafael Lima Dalmagro <rafa.dalmagro@hotmail.com>
2025-09-10 12:57:51 -0300

    Iniciando com css modules.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { Button } from "./components/button.tsx";
+import "./global.css";
+
+import { Button } from "./components/button";
+
+import "./style.css";
 
 export function App() {
     return (
-        <div>
+        <div className="container">
             <Button name="Salvar" onClick={() => alert("Clicado")} />
             <Button name="Cancelar" />
             <Button name="Excluir" />
